bb629a233e26833e353681e04dd225f55372c8d1
Adriel Joshua <joshuaamengual9305@gmail.com>
2025-09-08 12:00:48 -0300

    agrego get and set
    Signed-off-by: Adriel Joshua <joshuaamengual9305@gmail.com>

--- src/model/Socio.java
+++ src/model/Socio.java
@@ -53,4 +53,36 @@ public class Socio {
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
+
+    public int getIdSocio() {
+        return idSocio;
+    }
+
+    public void setIdSocio(int idSocio) {
+        this.idSocio = idSocio;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
 }
